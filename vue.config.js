538f4c2ea6b3775f4cc2f3c43597a385178ff5ff
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-12-14 17:30:30 +0100

    Remove loaderOptions from vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  'css': {
-    'loaderOptions': {
-      'sass': {
-        //'prependData': '@import "@/styles/_global.scss";'
-        //'@import "@/styles/_variables.scss";`
-      }
-    }
-  },
   'transpileDependencies': [
     'vuetify'
   ]
