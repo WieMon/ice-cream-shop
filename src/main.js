9ec4ebecfb2ef063053908b2e023b3d8a6d600c4
Monika Wieczorek <monikawieczoreq@gmail.com>
2021-01-03 19:12:35 +0100

    Add Vue-carousel

--- src/main.js
+++ src/main.js
@@ -4,20 +4,22 @@ import router from './router';
 import store from './store';
 import VueResource from 'vue-resource';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faIceCream, faUser, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faIceCream, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import vuetify from './plugins/vuetify';
 import { MdCard } from 'vue-material/dist/components';
 import wysiwyg from 'vue-wysiwyg';
 import vuelidate from 'vuelidate';
+import VueCarousel from 'vue-carousel';
 
-library.add(faIceCream, faUser, faChevronRight, faChevronLeft);
+library.add(faIceCream, faUser);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(VueResource);
 Vue.use(MdCard);
 Vue.use(wysiwyg, {maxHeight: '500px',});
 Vue.use(vuelidate);
+Vue.use(VueCarousel);
 
 Vue.http.options.root = 'https://ice-cream-shop-9b22e-default-rtdb.firebaseio.com';
 Vue.config.productionTip = false;
--- src/views/Home/HomeSlider.vue
+++ src/views/Home/HomeSlider.vue
@@ -1,48 +1,33 @@
 <template>
   <v-container>
     <v-row class='row'>
-      <image-slider>
-        <div class='slider__div' v-for='(slide, number) in [slides]' :key='number'>
-          <img  
-            class='slider__img' 
-            :src='currentImage' 
-          />
-          <div class='slider__icon-div'>
-            <a @click="prevImage" href='#'><font-awesome-icon class='slider__icon' icon='chevron-left' /></a>
-            <a @click="nextImage" href='#'><font-awesome-icon class='slider__icon' icon='chevron-right' /></a>
-          </div>
-          <p class='slider__text'
-            :src='currentDescription'>
-              {{currentDescription}}
-          </p>
-        </div>
-      </image-slider>
+      <div class='slider__div'>
+        <carousel 
+          :per-page='1' 
+          :mouse-drag='true' 
+          :navigationEnabled='true'
+          :paginationEnabled='false'
+          :loop='true'
+          :centerMode='true'
+        >
+          <slide v-for='(slide,index) in slides' :key='index'>
+            <img class='slider__img' :src="require(`../../assets/slider/${slide.img}`)"> 
+            <p class='slider__text'>{{slide.text}}</p>
+          </slide>
+      </carousel>
+      </div>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel';
+
 export default {
   data() {
     return {
       slides: [],
-      currentNumber: 0,
     };
-  },
-  mounted: function() {
-   this.startSlider();
-   
-  },
-  methods: {
-    startSlider: function() {
-      this.nextImage();
-    },
-    nextImage: function() {
-      this.currentNumber += 1;
-    },
-    prevImage: function() {
-      this.currentNumber -= 1;
-    }
   },
   created() {
     this.$http.get('slides.json')
@@ -59,13 +44,9 @@ export default {
       this.slides = list;
     });
   },
-  computed: {
-    currentImage: function() {
-      return require(`../../assets/slider/${this.slides[Math.abs(this.currentNumber) % this.slides.length].img}`);
-    },
-    currentDescription: function() {
-      return this.slides[Math.abs(this.currentNumber) % this.slides.length].text;
-    }
-  },
+  components: {
+    Slide,
+    Carousel
+  }
 };
 </script>
